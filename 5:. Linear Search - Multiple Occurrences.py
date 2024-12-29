48377611825bee909125e05ad3d1db4bd2c9965e
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2024-12-29 21:18:46 +0530

    Update 5:. Linear Search - Multiple Occurrences.py

--- 5:. Linear Search - Multiple Occurrences.py
+++ 5:. Linear Search - Multiple Occurrences.py
@@ -1,48 +1,33 @@
 '''
 Q 5. Linear Search - Multiple Occurences
-Solved
-feature icon
-Using hints except Complete Solution is Penalty free now
-Use Hint
-Problem Description
 
+Problem Description
 Given an array A and an integer B, find the number of occurrences of B in A.
 
-
 Problem Constraints
-
 1 <= B, Ai <= 109
 1 <= length(A) <= 105
 
 
 Input Format
-
 Given an integer array A and an integer B.
 
-
 Output Format
-
 Return an integer, number of occurrences of B in A.
 
-
 Example Input
-
 Input 1:
  A = [1, 2, 2], B = 2 
 Input 2:
  A = [1, 2, 1], B = 3 
 
-
 Example Output
-
 Output 1:
  2
 Output 2:
  0
 
-
 Example Explanation
-
 Explanation 1:
 Element at index 2, 3 is equal to 2 hence count is 2.
 Explanation 2:
