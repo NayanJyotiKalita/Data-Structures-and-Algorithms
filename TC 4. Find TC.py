6ee8484bd9edca5e8a88dce0e736fbac88b94e1c
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-03 22:47:26 +0530

    Create TC 4. Find TC.py

---
+++ TC 4. Find TC.py
@@ -0,0 +1,18 @@
+Find Time Complexity - 5
+
+def func(n):
+  s = 0
+  i = 0
+  while i < n:
+    s = s + i
+    i = i * 2
+  return s
+
+Options:
+1. O(n)
+2. O(n^(1/2))
+3. O(logn)
+4. O(infinite)
+
+Ans:
+4
