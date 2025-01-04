23a15bdf2389bdcb764b3c82a47643082e18a30d
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-04 22:40:41 +0530

    Create TC 9. Find TC.py

---
+++ TC 9. Find TC.py
@@ -0,0 +1,19 @@
+What is the time complexity of the following code :
+
+def solve():
+  i = 1
+  while i < n:
+    x = i
+    while x > 0:
+      x -= 1
+    i += 1
+
+Options:
+1. O(nlogn)
+2. O(n)
+3. O(n sqrt(n))
+4. O(n ^ 2)
+5. None of the above
+
+Ans:
+4
