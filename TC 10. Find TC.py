c97c04e09e84ca812ae0286abb79f73ad17ba4fd
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-04 22:42:13 +0530

    Create TC 10. Find TC.py

---
+++ TC 10. Find TC.py
@@ -0,0 +1,15 @@
+What is the time complexity of the following code :
+
+for i in range(N):
+  for j in range(i, N):
+    break
+
+Options:
+1. O(N * N)
+2. O(N * log(N))
+3. O(N)
+4. O(log(N))
+5. None of the above
+
+Ans:
+3
