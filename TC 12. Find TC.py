19c8d81740dfae5ed24567f8e6d1d74c5eb29c4c
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-05 11:44:14 +0530

    Create TC 12. Find TC.py

---
+++ TC 12. Find TC.py
@@ -0,0 +1,15 @@
+What is the time complexity of the following code:
+
+a = 0
+for i in range(N):
+  for j in range(N):
+    a = a + i + j
+
+Options:
+1. O(N)
+2. O(N * log(N))
+3. O(N * sqrt(N))
+4. O(N * N)
+
+Ans:
+4
