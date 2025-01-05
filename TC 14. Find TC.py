f371ed19470b0f6b074b7e42ddfa4b373d57c406
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-05 11:48:13 +0530

    Create TC 14. Find TC.py

---
+++ TC 14. Find TC.py
@@ -0,0 +1,16 @@
+What is the time complexity of the following code snippet
+
+for(int i = 1 ; i <= n ; i++){
+  for(int j = 1 ; j <= 3^i ; j++){
+    print(i + j);
+  }
+}
+
+Options:
+1. O(n ^ 2)
+2. O(nlogn)
+3. O(2 ^ n)
+4. O(3 ^ n)
+
+Ans:
+4
