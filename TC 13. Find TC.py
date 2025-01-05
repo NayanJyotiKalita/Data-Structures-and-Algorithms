d55e450d0fdc5a5d0644e85d6e478cfbdfe75ac0
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-05 11:46:50 +0530

    Create TC 13. Find TC.py

---
+++ TC 13. Find TC.py
@@ -0,0 +1,18 @@
+What is the time complexity of the following code snippet
+
+int func(int n){
+  int s = 0;
+  for(int i = 1 ; i*i*i <= n ; i++){
+    s = s + i;
+  }
+  return s;
+}
+
+Options:
+1. O(n ^ (1/4))
+2. O(n ^ (1/3))
+3. O(n ^ (1/2))
+4. O(n)
+
+Ans:
+2
