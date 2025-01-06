2b4dc6de3a068ff3d973b9e42a512c29f9eefa8e
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-06 22:40:42 +0530

    Create TC 19. Find TC.py

---
+++ TC 19. Find TC.py
@@ -0,0 +1,17 @@
+What is the time complexity of the following code snippet?
+
+for i in range(n):
+  j = i - 1
+  while j >= 0:
+    ans += i + j
+    j += 1
+
+Options:
+1. O(n)
+2. O(n ^ 2)
+3. Code will run indefinitely
+4. O(1)
+5. None of the above
+
+Ans:
+3
