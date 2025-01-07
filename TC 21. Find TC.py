9f448f0cd6264c49a3f0d70c5432f757dee0c426
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-07 18:41:30 +0530

    Create TC 21. Find TC.py

---
+++ TC 21. Find TC.py
@@ -0,0 +1,17 @@
+Find the Time Complexity of the following function solve :
+
+def solve(N):
+  for i in range(2 ** N):
+    j = i
+    while j > 0:
+      j -= 1
+
+Options:
+1. O(N * N)
+2. O(2 ^ N)
+3. O(N * (2 ^ N))
+4. O(3 ^ N)
+5. O(4 ^ N)
+
+Ans:
+5
