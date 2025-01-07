bf6a3d9b845ebe834649d2d729307583c6a1b5cd
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-07 18:38:15 +0530

    Create TC 20. Find TC.py

---
+++ TC 20. Find TC.py
@@ -0,0 +1,19 @@
+What is the time, space complexity of following code :
+
+a = 0
+b = 0
+for i in range(N):
+  for j in range(N):
+    a = a + j
+for k in range(N):
+  b = b + k
+
+Options:
+1. O(N * N) time, O(1) space
+2. O(N) time, O(N) space
+3. O(N) time, O(1) space
+4. O(N * N) time, O(N) space
+5. O(N * N * N) time, O(1) space
+
+Ans:
+1
