05645e2d2bec7bb7abe9aa13fa286c6b5c64cbd9
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-07 18:43:36 +0530

    Create TC 22. Find TC.py

---
+++ TC 22. Find TC.py
@@ -0,0 +1,16 @@
+What is the complexity of the following code snippet ?
+
+ans = 0
+for i in range(n):
+  ans += i * i
+return ans
+
+Options:
+1. O(nlogn)
+2. O(n)
+3. O(n ^ 2)
+4. O(1)
+5. None of the above
+
+Ans:
+2
