f954b744bb6e3fe046d190e115a20f172c62093f
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-08 13:21:56 +0530

    Update TC 3. Find TC.py

--- TC 3. Find TC.py
+++ TC 3. Find TC.py
@@ -1,10 +1,18 @@
-Find Time Complexity - 3
-Solved
-feature icon
-Using hints except Complete Solution is Penalty free now
-Use Hint
+'''
 What is the time complexity of the following code :
+'''
 
-C++
-Java
-Python
+def func(n):
+  s = 0
+  for i in range(1, 101):
+    s = s + i
+  return s
+
+Options:
+1. O(n)
+2. O(n ^ 2)
+3. O(s)
+4. O(1)
+
+Ans:
+1
