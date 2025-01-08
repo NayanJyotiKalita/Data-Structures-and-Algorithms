0f17a6a3a640a2cc9cfdc92a03141771ab17041c
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-08 13:15:36 +0530

    Create TC 24. Find TC.py

---
+++ TC 24. Find TC.py
@@ -0,0 +1,16 @@
+What will be the time complexity of the above function where n is a positive integer?
+
+def function(n):
+  while n > 0:
+    n += 1
+    n -= 2
+
+Options:
+1. Infinite loop  
+2. O(n)
+3. O(nlogn)
+4. O(logn)
+5. None of the above
+
+Ans:
+2
