28df47e00f3c392fb77e86ff1bc45cbaf3080c70
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-09 12:46:05 +0530

    Update TC Arrays 1.py

--- TC Arrays 1.py
+++ TC Arrays 1.py
@@ -1,3 +1,12 @@
-Time Complexity - Arrays
-
+'''
 What is the time complexity for inserting/deleting at the beginning of the array?
+'''
+
+Options:
+1. O(1)
+2. O(N)
+3. O(logN)
+4. O(N ^ 2)
+
+Ans:
+2
