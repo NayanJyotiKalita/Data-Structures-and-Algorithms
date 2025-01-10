edd383d117b59d4e87dc76eef0aeb93d56f91e9c
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-10 14:36:49 +0530

    Create PrefSum 1.py

---
+++ PrefSum 1.py
@@ -0,0 +1,14 @@
+The prefix sum formula for an array A of N integers is given by :-
+
+Options:
+       
+1. prefSum[i] = prefSum[i] + A[i]
+
+2. prefSum[i] = prefSum[i - 1] + A[i]
+
+3. prefSum[i - 1] = prefSum[i] + A[i]
+
+4. prefSum[i - 1] = prefSum[i - 1] + A[i]
+
+Ans:
+prefSum[i] = prefSum[i - 1] + A[i]
