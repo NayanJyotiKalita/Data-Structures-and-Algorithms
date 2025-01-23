6f9fd85dd3e4ca849fa43d212ef857849e44bdec
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-23 22:50:46 +0530

    Create Sorting 1 - TC.py

---
+++ Sorting 1 - TC.py
@@ -0,0 +1,19 @@
+What is the time complexity of the following code snippet?
+
+v = []
+for i in range(n):
+  v.append(i)
+
+v.sort()
+
+Options:
+
+1. O(n)
+
+2. O(nlogn)
+
+3. O(1)
+
+4. O(n^2)
+
+5. None of the above
