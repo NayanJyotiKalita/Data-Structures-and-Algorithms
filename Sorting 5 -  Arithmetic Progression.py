9eb0650ee1c4bb6b0698b1a9025ae6983c546c3a
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-01-26 18:13:50 +0530

    Update Sorting 5 -  Arithmetic Progression.py

--- Sorting 5 -  Arithmetic Progression.py
+++ Sorting 5 -  Arithmetic Progression.py
@@ -34,3 +34,13 @@ Explanation 2:
  There is no way to reorder the elements to obtain an arithmetic progression.
 
 CODE:
+
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, A):
+        A.sort()
+        for i in range(1,len(A)-1):
+            if A[i] - A[i-1] != A[i+1] - A[i]:
+                return 0
+        return 1
