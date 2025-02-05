349c7cba81dd212eb8028b66911f86d8b6614e39
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-05 23:34:19 +0530

    Update 2. IsPrime.py

--- 2. IsPrime.py
+++ 2. IsPrime.py
@@ -8,13 +8,41 @@ The value of A can cross the range of Integer.
 Problem Constraints
 1 <= A <= 109
 
+Input Format
+The first argument is a single integer A.
+
+Output Format
+Return 1 if A is prime else return 0.
+
+Example Input
+Input 1:
+A = 5
+Input 2:
+A = 10
+
+Example Output
+Output 1:
+1
+Output 2:
+0
+
+Example Explanation
+Explanation 1:
+5 is a prime number.
+Explanation 2:
+10 is not a prime number.
+
+
 CODE:
-def solve(self, A):
-    i = 2
-    while i * i <= A:
-        if A % i == 0:
-            return 0
-        i += 1
-    return int(A >= 2)
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def isprime(self, A):
+        i = 2
+        while i * i <= A:
+            if A % i == 0:
+                return 0
+            i += 1
+        return int(A >= 2)
 
 solution = Solution()
