53dbc98271302e58cb102416b7b5c6d0af33ea99
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-07 12:10:25 +0530

    Create Intro to PS 3.py

---
+++ Intro to PS 3.py
@@ -0,0 +1,20 @@
+Find Iterations
+
+Find the number of times below code runs where N is a perfect square
+
+for i -> 1 to N
+  if(i * i == N)
+    return i
+
+Option:
+
+1. N
+
+2. N/2
+ 
+3. sqrt(N)
+
+4. logN
+
+Ans:
+3
