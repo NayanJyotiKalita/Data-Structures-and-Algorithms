7fbb453ce0ebd3e759997a2d6bb15ec7749ff33f
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-08 23:14:18 +0530

    Create Intro to PS - 5.py

---
+++ Intro to PS - 5.py
@@ -0,0 +1,24 @@
+Find Number of Iterations - 3
+
+How many iterations will take place?
+
+int func(int n){
+    int s = 0;
+    for(int i = 1 ; i <= 100 ; i++){ 
+        s = s + i;
+    }
+    return s;
+}
+
+Options:
+
+1. 100
+
+2. 99
+
+3. 98
+
+4. 101
+
+Ans:
+1
