f48411c9f1adea876690b3f00e7616e6056989d7
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-08 23:16:12 +0530

    Create Intro to PS 6.py

---
+++ Intro to PS 6.py
@@ -0,0 +1,22 @@
+Find Number of Iterations - i
+
+Find the number of iterations in the following code snippet:
+
+int ans = 0;
+for (int i = 0; i < n; i++)
+{
+    ans += i * i;
+}
+
+Options:
+
+1. n^2
+
+2. n
+
+3. n-1
+
+4. n+1
+
+Ans:
+2
