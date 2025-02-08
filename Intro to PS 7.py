b191c1895dc74584fa070d110476ae88845216bb
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-08 23:17:27 +0530

    Create Intro to PS 7.py

---
+++ Intro to PS 7.py
@@ -0,0 +1,22 @@
+Find Number of Iterations - 7
+
+What will be the total number of iterations in the following code?
+
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+        print(i + j);
+    }
+}
+
+Options:
+
+1. n^2
+
+2. n
+
+3. n*(n+1) / 2
+
+4. n*(n-1) / 2
+
+Ans:
+3
