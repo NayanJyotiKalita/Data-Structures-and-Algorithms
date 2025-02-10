0e1344e2bef7f0c4cce12ece84964a614ae3ee51
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-10 23:14:30 +0530

    Create Intro to PS 11.py

---
+++ Intro to PS 11.py
@@ -0,0 +1,33 @@
+Find Number of Iterations - 2
+
+Find the total number of iterations in the following code:
+
+void solve(int N, int M)
+{
+    int count = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (N % i == 0)
+            count++;
+    }
+    for (int i = 1; i <= M; i++)
+    {
+
+        if (M % i == 0)
+            count++;
+    }
+    print count;
+}
+
+
+Choose the correct answer from below:
+1. N
+
+2. N + M
+
+3. M
+
+4. N/2 + M/2
+
+Ans:
+2
