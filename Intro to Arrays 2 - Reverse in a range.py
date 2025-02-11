0f78a3567f21f70fee772f3bb34aa611a1810df2
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-11 23:01:56 +0530

    Update Intro to Arrays 2 - Reverse in a range.py

--- Intro to Arrays 2 - Reverse in a range.py
+++ Intro to Arrays 2 - Reverse in a range.py
@@ -38,9 +38,26 @@ class Solution:
     # @param B : integer
     # @param C : integer
     # @return a list of integers
-    def solve(self, A, B, C):
+    def reverse_in_range(self, A, B, C):
         for i in range((C-B+1)//2):
             A[B+i], A[C-i] = A[C-i], A[B+i]
         return A
 
+'or'
+
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @param C : integer
+    # @return a list of integers
+    def reverse_in_range(self, A, B, C):
+        i , j = B , C
+        while i < j :
+            temp = A[i]
+            A[i] = A[j]
+            A[j] = temp
+            i , j = i + 1 , j - 1
+        return A
+
 solution = Solution()
+print(solution.reverse_in_range(A = [1, 2, 3, 4], B = 2, C = 3))
