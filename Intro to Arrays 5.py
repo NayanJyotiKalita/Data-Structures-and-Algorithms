f173ffdb7da52c6a6e45fcb712f73f9b66a3debd
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-13 15:58:53 +0530

    Create Intro to Arrays 5.py

---
+++ Intro to Arrays 5.py
@@ -0,0 +1,16 @@
+Time Complexity - Arrays
+
+What is the time complexity for inserting/deleting at the beginning of the array?
+
+Choose the correct answer from below:
+
+1. O(1)
+
+2. O(N)
+
+3. O(logN)
+
+4. O(N^2)
+
+Ans:
+2
