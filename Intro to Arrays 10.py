85c0459acd2f7d84d45fed9c8a5a87bb6fa5a222
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-14 22:50:15 +0530

    Create Intro to Arrays 10.py

---
+++ Intro to Arrays 10.py
@@ -0,0 +1,32 @@
+Problems on 1D Arrays-2 MCQ D
+
+Raw Problem
+
+What will be the output of the following code?
+
+class Main {
+   static void fun(int[]arr) {
+       arr[3] = 98;
+       return;
+   }
+   public static void main(String args[]) {
+       int[]arr = {10,20,30,40,50};
+       fun(arr);
+       System.out.println(arr[3]);
+   }
+}
+
+
+Choose the correct answer from below:
+
+1. 40
+
+2. 30
+
+3. 98
+
+4. Error
+
+Ans:
+3
+
