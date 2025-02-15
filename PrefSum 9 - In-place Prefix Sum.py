d73fd638b5a69f335f94909fc59d25028d3b6da9
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-15 23:38:48 +0530

    Update PrefSum 9 - In-place Prefix Sum.py

--- PrefSum 9 - In-place Prefix Sum.py
+++ PrefSum 9 - In-place Prefix Sum.py
@@ -36,9 +36,10 @@ CODE:
 class Solution:
     # @param A : list of integers
     # @return a list of integers
-    def solve(self, A):
+    def in_place_psum(self, A):
         for i in range(1, len(A)):
             A[i] = A[i] + A[i-1]
         return A
 
 solution = Solution()
+print(solution.in_place_psum(A = [1, 2, 3, 4, 5]))
