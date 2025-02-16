472b7f076e5091627bdd69fd5f479c3b5aaf55bb
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-16 18:12:13 +0530

    Update Carry Fwd 7 - Best Time to Buy and Sell Stocks 1.py

--- Carry Fwd 7 - Best Time to Buy and Sell Stocks 1.py
+++ Carry Fwd 7 - Best Time to Buy and Sell Stocks 1.py
@@ -1,6 +1,5 @@
 Best Time to Buy and Sell Stocks I
 
-
 Problem Description
 Say you have an array, A, for which the ith element is the price of a given stock on day i.
 If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
@@ -69,3 +68,4 @@ class Solution:
 
 
 solution = Solution()
+print(solution.maxProfit(A = [1, 4, 5, 2, 4]))
