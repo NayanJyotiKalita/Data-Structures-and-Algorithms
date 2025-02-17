e64e67cbcdaa0ee86bb8d35073931ac35fd8a50d
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-17 23:10:53 +0530

    Update Sliding 1 - Maximum Subarray Easy.py

--- Sliding 1 - Maximum Subarray Easy.py
+++ Sliding 1 - Maximum Subarray Easy.py
@@ -1,6 +1,5 @@
 Maximum Subarray Easy
 
-
 Problem Description
 You are given an integer array C of size A. Now you need to find a subarray (contiguous elements) so that the sum of contiguous elements is maximum.
 But the sum must not exceed B.
@@ -59,3 +58,4 @@ class Solution:
         return summ
 
 solution = Solution()
+print(solution.maxSubarray(A = 5, B = 12, C = [2, 1, 3, 4, 5]))
