876363569e055aac677834495cbdea0b61953ebb
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-25 23:21:17 +0530

    Create Bit Manipulations Basics 2.py

---
+++ Bit Manipulations Basics 2.py
@@ -0,0 +1,20 @@
+Binary to Decimal
+
+In our base-10 system, the positions (from right to left) correspond to the "number of 1s", "number of 10s", "number of 100s", and so on, where the value of each position is 10 times the previous one.
+
+Binary, or base 2, uses only the digits 0 and 1. Each position corresponds to the "number of 1s", "number of 2s", "number of 4s", "number of 8s", and so on. The value of each position is 2 times the previous one.
+
+The number mentioned above if expressed in base 10 will give 8 + 1 = 9 since there is one 8 and one 1.
+
+What is the value (in base 10) of the binary number 10000?
+
+Choose the correct answer from below:
+12
+
+14
+
+16
+
+20
+
+8
