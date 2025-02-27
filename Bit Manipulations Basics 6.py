24800cb698c11248d682e582edf47264e3bf5056
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-27 23:07:40 +0530

    Create Bit Manipulations Basics 6.py

---
+++ Bit Manipulations Basics 6.py
@@ -0,0 +1,16 @@
+Bit Operations - 5
+
+What are the values of a&a, a|a, a^a?
+
+Choose the correct answer from below:
+
+1. a&a = a , a|a = a , a^a = a
+
+2. a&a = a , a|a = a , a^a = 0
+
+3. a&a = 0 , a|a = a , a^a = a
+
+4. a&a = 0 , a|a = a , a^a = 0
+
+Ans:
+2
