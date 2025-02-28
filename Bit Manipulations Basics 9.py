79792c113eef7dfca565e1c8f8f4b6ca328d0fa0
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-02-28 23:27:22 +0530

    Create Bit Manipulations Basics 9.py

---
+++ Bit Manipulations Basics 9.py
@@ -0,0 +1,13 @@
+Bit Operations - 3
+
+What happens to the 0-th bit in a when we perform a = a^1 ?
+
+Choose the correct answer from below:
+
+1. It gets toggled
+
+2. It gets unset
+
+3. It gets set
+
+4. It remains same
