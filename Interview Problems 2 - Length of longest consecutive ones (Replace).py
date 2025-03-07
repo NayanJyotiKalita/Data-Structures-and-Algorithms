3d395c0da73eb8ac0a959ad7266c4d3e4fb2a8cc
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-03-07 22:59:53 +0530

    Create Interview Problems 2 - Length of longest consecutive ones (Replace).py

---
+++ Interview Problems 2 - Length of longest consecutive ones (Replace).py
@@ -0,0 +1,28 @@
+Length of longest consecutive ones (Replace)
+
+Given a binary string A. It is allowed to do at most one replacement of 0 by 1. Find and return the length of the longest consecutive 1’s that can be achieved.
+
+Input Format
+The only argument given is string A.
+
+Output Format
+Return the length of the longest consecutive 1’s that can be achieved.
+  
+Constraints
+1 <= length of string <= 1000000
+A contains only characters 0 and 1.
+
+For Example
+
+Input 1:
+    A = "111000"
+Output 1:
+    4
+
+Input 2:
+    A = "111011101"
+Output 2:
+    7
+
+CODE:
+
