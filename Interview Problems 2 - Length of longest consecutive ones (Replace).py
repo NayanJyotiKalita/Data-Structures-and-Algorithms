1fd56828929ecebaf13174ebd819e38427a1e009
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-03-09 22:30:24 +0530

    Update Interview Problems 2 - Length of longest consecutive ones (Replace).py

--- Interview Problems 2 - Length of longest consecutive ones (Replace).py
+++ Interview Problems 2 - Length of longest consecutive ones (Replace).py
@@ -1,6 +1,8 @@
 Length of longest consecutive ones (Replace)
 
 Given a binary string A. It is allowed to do at most one replacement of 0 by 1. Find and return the length of the longest consecutive 1’s that can be achieved.
+'or'
+Given a binary string A. We can almost replace a single 0 with 1. Find the maximum consecutive 1's we can get in the string after the replacement.
 
 Input Format
 The only argument given is string A.
@@ -26,3 +28,22 @@ Output 2:
 
 CODE:
 
+class Solution:
+    def longest_consecutive_ones_replace(self, A):
+          ans = 0
+          n = len(A)
+          for i in range(n):
+                if A[i] == 0:
+                      l, r, = 0, 0
+                      j = i - 1
+                      while j >= 0 and A[j] == 1:
+                            l += 1
+                            j -= 1
+                      j = i + 1
+                      while j < n and A[j] == 1:
+                            r += 1
+                            j += 1
+                      ans = max(ans, l + r + 1)
+          if ans == 0 and n > 0:
+                ans = n
+          return ans
