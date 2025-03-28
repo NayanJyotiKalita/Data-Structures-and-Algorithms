260b8494273db136fd1836912722b485be08901a
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-03-28 13:53:30 +0530

    Create Bit Manipulations 6 - Bit Operations MCQ.py

---
+++ Bit Manipulations 6 - Bit Operations MCQ.py
@@ -0,0 +1,15 @@
+Bit Operations
+
+What are the values of a&a, a|a, a^a?
+
+Choose the correct answer from below:
+1. a&a = a , a|a = a , a^a = a
+
+2. a&a = a , a|a = a , a^a = 0
+
+3. a&a = 0 , a|a = a , a^a = a
+
+4. a&a = 0 , a|a = a , a^a = 0
+
+Ans:
+a&a = a , a|a = a , a^a = 0
