12972022fc42bbff29dcf03391f4c11a7e36829e
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-03-29 22:41:00 +0530

    Update Bit Manipulations 2 - Single Number.py

--- Bit Manipulations 2 - Single Number.py
+++ Bit Manipulations 2 - Single Number.py
@@ -39,6 +39,17 @@ class Solution:
     # @return an integer
     def singleNumber(self, A):
         ans = 0
+        for i in range(len(A)):
+            ans = ans ^ A[i]
+        return ans
+
+'or'
+
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def singleNumber(self, A):
+        ans = 0
         for i in range(32):
             count = 0
             for j in range(len(A)):
