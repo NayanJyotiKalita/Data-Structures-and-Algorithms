a372b1d163fa561243392e882469a1e428e82abe
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-04-20 23:06:17 +0530

    Create DSA 1 Contest 4 - Highest Product.py

---
+++ DSA 1 Contest 4 - Highest Product.py
@@ -0,0 +1,35 @@
+Highest Product (Greedy Algorithm)
+
+Problem Description
+Given an integer array A of size N.
+Return the highest product possible by multiplying 3 numbers from the array.
+NOTE: Solution will fit in a 32-bit signed integer.
+
+Problem Constraints
+1 <= N <= 5* 105
+
+Input Format
+The first and the only argument is an integer array A.
+
+Output Format
+Return an integer denoting the highest possible product.
+
+Example Input
+A = [0, -1, 3, 100, 70, 50]
+Example Output
+350000
+
+Example Explanation
+70 * 50 * 100 = 350000
+
+CODE
+
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def maxp3(self, A):
+        A.sort()
+        n = len(A)
+        p1 = A[n-1] * A[n-2] * A[n-3]
+        p2 = A[0] * A[1] * A[n-1]
+        return max(p1, p2) 
