61e2bc60ded728e2fe9a20ddd7e0a72bb6248f3d
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-04-21 23:40:00 +0530

    Update Intro to Arrays 2 - Reverse in a range.py

--- Intro to Arrays 2 - Reverse in a range.py
+++ Intro to Arrays 2 - Reverse in a range.py
@@ -1,4 +1,4 @@
-Q. Reverse in a range
+Reverse in a range
 
 Problem Description
 Given an array A of N integers and also given two integers B and C. Reverse the elements of the array A within the given inclusive range [B, C].
