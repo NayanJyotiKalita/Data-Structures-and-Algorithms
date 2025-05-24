4580626e6a0c805ffe76ae9d95e2c89f1f6515d7
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2025-05-24 23:28:30 +0530

    Update 1D Array 2 - Max Sum Contiguous Subarray.py

--- 1D Array 2 - Max Sum Contiguous Subarray.py
+++ 1D Array 2 - Max Sum Contiguous Subarray.py
@@ -46,4 +46,4 @@ class Solution:
 
 
 solution = Solution()
-print(solution.maxSubArray(A = [-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+print(solution.maxSubArray(A = [-2, 1, -3, 4, -1, 2, 1, -5, 4]))  -->  O/P: 6
